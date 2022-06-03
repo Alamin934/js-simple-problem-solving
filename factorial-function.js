09d363cc0e34062894a14c94adf37f26a7b3f297
Alamin934 <islamalamin934@gmail.com>
2022-06-03 14:01:05 +0600

    added functions value

--- factorial-function.js
+++ factorial-function.js
@@ -2,11 +2,11 @@
 
 function getFactorial(number) {
     let factorial = 1;
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= number; i++) {
         factorial = factorial * i;
         console.log(i, factorial);
     }
     console.log(factorial);
 }
 
-getFactorial(7);
+getFactorial(5);
