825befb4b29c1c76349e351612867407d60396d6
Alamin934 <islamalamin934@gmail.com>
2022-06-03 14:38:25 +0600

    decrement factorial using while loop

--- factorial-while-loop.js
+++ factorial-while-loop.js
@@ -14,25 +14,25 @@ console.log(myFactorial);
 */
 
 // dicrement value of factorial
-// function getFactorial(number) {
+
+function decrementFactorial(number) {
+    let factorial = 1;
+    let i = number;
+    while (i >= 1) {
+        factorial = factorial * i;
+        i--;
+    }
+    console.log(factorial);
+}
+decrementFactorial(7);
+
+
+// function getFactorial2(number) {
 //     let factorial = 1;
-//     let i = number;
-//     while (i >= 1) {
+//     for (let i = number; i >= 1; i--) {
 //         factorial = factorial * i;
-//         i--;
 //     }
 //     return factorial;
 // }
-
-// let myFactorial = getFactorial(6);
+// let myFactorial = getFactorial2(6);
 // console.log(myFactorial);
-
-function getFactorial2(number) {
-    let factorial = 1;
-    for (let i = number; i >= 1; i--) {
-        factorial = factorial * i;
-    }
-    return factorial;
-}
-let myFactorial = getFactorial2(6);
-console.log(myFactorial);
